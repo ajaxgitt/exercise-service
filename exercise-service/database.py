a9f370cc9d6dab4aa1a4a8d8f4266a12776b62f2
ajaxgitt <164401823+ajaxgitt@users.noreply.github.com>
2024-11-19 22:20:06 -0400

    configuracion msql

--- exercise-service/database.py
+++ exercise-service/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #  mysql+mysqldb://root:password@mysql:3307/problems
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:password@mysql:3306/problems"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:password@mysql:3306/problems"
+SQLALCHEMY_DATABASE_URL = "mysql://root:SXFKCZuveyZKuniVSUjuKNBaFqjxnLed@junction.proxy.rlwy.net:45969/railway"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
