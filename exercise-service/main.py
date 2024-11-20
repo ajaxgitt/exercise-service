3140d050592a532f974bdb3bbbbaa008dd4cf2bf
ajaxgitt <164401823+ajaxgitt@users.noreply.github.com>
2024-11-20 00:55:28 -0400

    adf

--- exercise-service/main.py
+++ exercise-service/main.py
@@ -11,12 +11,13 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 origins = [
     "http://127.0.0.1:8000",
     "http://localhost:5173",
+    
 ]
 
 #hlii
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
